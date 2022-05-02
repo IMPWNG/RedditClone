e169d612af5314c5a3b8828ecb79fec559029821
IMPWNG <matisse.ach+github.com>
2022-05-02 18:57:54 -0400

    TODO: Add bg banner sub

--- client/src/components/Ui/SubHeader/SubBanner.jsx
+++ client/src/components/Ui/SubHeader/SubBanner.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 export default function SubBanner() {
   return (
     <>
+    {/* Add SubBanner background */}
       <section className="h-20 bg-cover" style={{backgroundImage:'url("../assets/banner/redditBanner.jpg")'}}>
         <h1>test</h1>
       </section>
