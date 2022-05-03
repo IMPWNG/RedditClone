04a3a3322808df6b8244888e364acc391955815e
IMPWNG <matisse.ach+github.com>
2022-05-03 11:04:06 -0400

    FixAvatarStyleHeader🔨

--- client/src/components/Ui/Header/UserAvatar.jsx
+++ client/src/components/Ui/Header/UserAvatar.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import { ChevronDownIcon } from '@heroicons/react/solid';
 import UserImage from '../../../assets/userAvatar/defaultUser.png';
 
 
 export default function UserAvatar() {
   return (
     <>
-      <button className="rounded-md flex ml-4">
-        <div className="w-8 h-8 bg-gray-600 rounded-md">
-          <img src={UserImage} alt="" className="block" />
-          <ChevronDownIcon className="text-gray-500 w-5 h-5 mt-2 ml-1" />
+      <button className="px-2 py-1">
+        <div className="w-8 h-8 bg-gray-600 rounded-md m-1 mx-2">
+          <img src={UserImage} alt="" className="" />
         </div>
       </button>
     </>
