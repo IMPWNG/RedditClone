5fe948b563ae87caec0d65862d39b989ec5a5c1c
IMPWNG <matisse.ach+github.com>
2022-05-05 12:26:34 -0400

    serverLogic

--- server/server.js
+++ server/server.js
@@ -1,28 +1,41 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const cors = require("cors");
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-// parse requests of content-type - application/json
 
-app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json('Welcome to CORS server 😁')
-})
+app.use(cookieParser());
+
+// Enable CORS for all HTTP methods - FrontEnd Origin
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 
-// simple route
+// await SupabaseClient({ url: process.env.SUPABASE_URL });
+
+
+// simples routes
+app.get("/", (req, res) => {
+  res.json("Welcome to CORS server 😁");
+});
 app.get("/cors", (req, res) => {
   res.json({ message: "This has CORS enabled 🎈" });
 });
-
-
 
 // set port, listen for requests
 app.listen(PORT, () => {
